23d514b9dbf70d1dd24e583f6db843fa5656361a
mathieumcveety <mathieumcveety@gmail.com>
2024-10-19 14:19:50 -0400

    styling for 404 page

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -4,9 +4,11 @@ import "./NotFound.style.scss";
 function NotFound() {
   return (
     <Layout title={"Page Not Found"}>
-      <h1>Error 404: Page Does Not Exist</h1>
-      <h2>You're cooked</h2>
-      <img src="../../../public/cooked.gif" alt="cooked"></img> 
+      <div className="center">
+        <h1>Error 404: Page Does Not Exist</h1>
+        <h2>You're cooked</h2>
+        <img src="../../../public/cooked.gif" alt="cooked"></img>
+      </div>
     </Layout>
   );
 }
